85654f570a86c38b33ca125aaf048ff97de021b7
philippedehovre <me@philippedehovre.com>
2024-01-15 09:36:12 +0000

    kinde env modified

--- app/dashboard/[resource]/[id]/page.tsx
+++ app/dashboard/[resource]/[id]/page.tsx
@@ -18,8 +18,12 @@ import Spinner from "@components/Spinner";
 
 const Page = async ({ params }: any) => {
   const { getUser } = getKindeServerSession();
+  const { getIdToken } = getKindeServerSession();
+  const tokens = await getIdToken();
+  console.log(tokens);
 
   const user = (await getUser()) as KindeUser;
+  console.log(user);
   const resource =
     params.resource === "template"
       ? ((await getUniqueTemplateByUser(params.id, user)) as TemplateType)
